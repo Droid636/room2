65c0902e98bb9a527ed4f2a45db422365a284a36
Droid636 <2022043@utsh.edu.mx>
2024-10-11 17:07:29 -0600

    avance

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,25 @@
+import Link from "next/link";
 import React from "react";
-
 
 
 export default function Login() {
   return (
     <div className="login-container">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
       <div className="login-box">
         <h2 className="login-title">Iniciar sesión</h2>
         <form className="login-form">
           <div className="form-group">
             <label htmlFor="email">Correo electrónico</label>
-            <input type="email" id="email" value="" required />
+            <input type="email" id="email" placeholder="Ingrese su correo" required />
           </div>
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" id="password" value="" required />
+            <input type="password" id="password" placeholder="Ingrese su contraseña" required />
           </div>
           <button type="submit" className="login-button">Iniciar</button>
         </form>
         <p className="register-link">
-          ¿No tienes una cuenta? <a href="/fonts">Regístrate</a>
+          ¿No tienes una cuenta? <Link href="/register" style={{color: 'black'}}>xd</Link> 
         </p>
       </div>
     </div>
---
+++ app/register/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+
+function use() {
+    return (
+      <div>
+        <h1>hola</h1>        
+      </div>
+    )
+  }
+  
+  export default use;
