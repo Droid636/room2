0cac8d92b6cd4a208df5b2f85c99198205e06294
Droid636 <2022043@utsh.edu.mx>
2024-11-05 17:51:43 -0600

    home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,32 +3,47 @@ import "@/app/home/home.css";
 
 function HomePage() {
   return (
-    <div>
-      <div className="card">
-        <h3>card 1</h3>
-        <label htmlFor="modalToggle" className="open-modal-label">Click to open modal</label>
-      </div>
+    <div className="home-container">
+      <h1 className="welcome-title">Bienvenido</h1>
+      <div className="card-container">
+        {/* Card 1 que activa el modal */}
+        <label htmlFor="modalToggle" className="card clickable-card">
+          <h3>Ingresa tu Ubicacion</h3>
+          
+        </label>
 
-      <div className="card">
-        <h3>card 2</h3>
-        <p>Content of card 2</p>
-      </div>
+        <div className="card">
+          <h3>Card 2</h3>
+          <p>Contenido del card 2.</p>
+        </div>
 
-      <div className="card">
-        <h3>card 3</h3>
-        <p>Content of card 3</p>
-      </div>
-
-      {/* Modal Structure */}
-      <input type="checkbox" id="modalToggle" className="modal-toggle" />
-      <div className="modal">
-        <label htmlFor="modalToggle" className="modal-background"></label> {/* Cierra el modal al hacer clic fuera */}
-        <div className="modal-content">
-          <label htmlFor="modalToggle" className="close-modal-label">&times;</label>
-          <h2>Modal Title</h2>
-          <p>This is the content of the modal.</p>
+        <div className="card">
+          <h3>Card 3</h3>
+          <p>Contenido del card 3.</p>
         </div>
       </div>
+
+      {/* Modal Estructura */}
+      <input type="checkbox" id="modalToggle" className="modal-toggle card" />
+      <div className="modal">
+  <label htmlFor="modalToggle" className="modal-background"></label>
+  <div className="modal-content">
+    <label htmlFor="modalToggle" className="close-modal-label">&times;</label>
+    <h2>Busca la colonia donde deseas rentar</h2>
+    
+    {/* Menú desplegable */}
+    <select className="options-dropdown" defaultValue="">
+      <option value="" disabled>Selecciona una opción</option>
+      <option value="1">Opción 1</option>
+      <option value="2">Opción 2</option>
+      <option value="3">Opción 3</option>
+    </select>
+    
+    {/* Botón de búsqueda */}
+    <button className="price-range-btn">Buscar</button>
+  </div>
+</div>
+
     </div>
   );
 }
