f7db73c433ca8faed5fda502e3a7a8ebcc8dda87
Droid636 <2022043@utsh.edu.mx>
2024-11-08 16:52:33 -0600

    home

--- app/nav.tsx
+++ app/nav.tsx
@@ -13,17 +13,15 @@ function Nav() {
   return (
     <header className="header">
       <div className="header-container">
-        <h3 className="header-title">My Website</h3>
+        <h3 className="header-title">Room for you</h3>
         <button className="menu-toggle" onClick={toggleMenu}>
           ☰
         </button>
       </div>
       <nav className={`menu ${isOpen ? 'open' : ''}`}>
         <ul className="menu-list">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="/home">Inicio</a></li>
+          <li><a href="/">Salir</a></li>
         </ul>
       </nav>
     </header>
